9fdafb34bf012758f04a3030cea72a753ba346ca
MaksimShumkov <shumkovmaxim@yandex.by>
2022-03-18 11:06:18 +0300

    Creating two classes for using Threads for Order class Order class CleanerOrder

---
+++ store/src/main/java/by/issoft/store/Order.java
@@ -0,0 +1,44 @@
+package by.issoft.store;
+
+import by.issoft.domain.Product;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Order implements Runnable {
+
+    private CleanOrder cleanOrder;
+    private final List<Product> productList;
+    private List<Product> orderProductList = new CopyOnWriteArrayList<>();
+
+    public Order(List<Product> productList) {
+        this.productList = productList;
+    }
+
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        Integer randomTime = new Random().ints(1, 1, 31).boxed().findAny().get();
+
+        synchronized (productList) {
+            log.info("start order thread, time of processing your order :" + randomTime);
+            productList.wait(randomTime * 1000);
+            cleanOrder = new CleanOrder(orderHandler(orderProductList));
+            (new Thread(cleanOrder)).start();
+        }
+    }
+
+    @SneakyThrows
+    private synchronized List<Product> orderHandler(List<Product> productList) {
+        orderProductList = this.productList;
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("Your order is completed :" + orderProductList.toString());
+        return orderProductList;
+    }
+}
